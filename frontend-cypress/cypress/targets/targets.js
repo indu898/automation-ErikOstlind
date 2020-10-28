a8e0ddcc75d483c43a9ad0e45a8aed4e32864691
Erik <erik.ostlind@gmail.comgit config --global user.name Erik>
2020-10-28 23:02:34 +0100

    added a target on your head

---
+++ frontend-cypress/cypress/targets/targets.js
@@ -0,0 +1,12 @@
+module.exports = {
+badUsername : 'Tester',
+badPassword : 'Verybadpassword',
+username : 'tester01',
+password :'GteteqbQQgSr88SwNExUQv2ydb7xuf8c',
+wrongCredentialsAlert : 'Bad username or password',
+dashboardContains : 'Welcome tester01!',
+baseUrl:'http://localhost:3000/login',
+clientName: 'Erik Erikson',
+clientEmail: 'erik@erikson.com',
+clientTelephone: '08258250'
+}
